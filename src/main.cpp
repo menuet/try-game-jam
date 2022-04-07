67cac226390ff998ba7d16242c089c56b644591c
Pascal Menuet <menuet@github.com>
2022-04-07 22:28:46 +0200

    replace try-game-jam by try_game_jam

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 #include <spdlog/spdlog.h>
 
 // This file will be generated automatically when you run the CMake
-// configuration step. It creates a namespace called `try-game-jam`. You can modify
+// configuration step. It creates a namespace called `try_game_jam`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
@@ -322,8 +322,8 @@ int main(int argc, const char **argv)
       { std::next(argv), std::next(argv, argc) },
       true,// show help if requested
       fmt::format("{} {}",
-        try-game-jam::cmake::project_name,
-        try-game-jam::cmake::project_version));// version string, acquired
+        try_game_jam::cmake::project_name,
+        try_game_jam::cmake::project_version));// version string, acquired
                                             // from config.hpp via CMake
 
     if (args["turn_based"].asBool()) {
